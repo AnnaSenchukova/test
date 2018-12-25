25f4d822d9a54529a640731931439447b1b3c04f
AnnaSenchukova <36882792+AnnaSenchukova@users.noreply.github.com>
2018-12-25 21:21:35 +0300

    style: Отредактированы файлы стилей

--- app/src/components/Navigation/index.js
+++ app/src/components/Navigation/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import Logo from '../Logo/index';
 import Search from '../Search/index'
 import UserNavigation from '../UserNavigation/index'
 import CatalogNavigation from '../CatalogNavigation/index'
@@ -11,8 +10,12 @@ function Navigation () {
 return (
         <nav className="navigation">
             <div className="navigation__wrapper container">
-
-             <Logo />
+            <a className="navigation__item logo logo--header">
+                <picture>
+                <source media="(min-width: 768px)" srcset={require("../../img/logo-orange.png")}/>
+                <img className="logo-image" src={require("../../img/logo-mobile.png")} alt="Логитип магазина SHOP24 New Look"/>
+                </picture>
+            </a>
              <Search />
              <UserNavigation />
 
